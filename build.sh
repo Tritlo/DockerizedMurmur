3fbb6988d9cb2c1d2919e585a87a4031ec907d34
Matthías Páll Gissurarson <mpg@mpg.is>
2015-11-10 15:27:59 +0100

    removed sh files

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build --rm -t mpgis/mumble .
--- run.sh
+++
@@ -1,3 +0,0 @@
-docker stop mumble
-docker rm mumble
-docker run -d --name mumble -p 64738:64738 -v $PWD/murmur/:/root/murmur/ mpgis/mumble
